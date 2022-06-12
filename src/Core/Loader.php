654ddcbb6a2e9f642624aead4a78f15538ee120e
parham <Parham_minoo@yahoo.com>
2022-06-12 05:27:40 -0700

    adding routers and finishing simple loader

--- src/Core/Loader.php
+++ src/Core/Loader.php
@@ -1,17 +1,32 @@
 <?php
 
 use App\Core\Router;
-require_once './Router.php';
+require_once 'Config.php';
 
 $router = new Router();
 
-$router->route('/', function(){
+$router->route('get',BASE_URI . '/', function(){
     echo 'hello world';
 });
 
-$router->route('/about', function(){
-    echo 'thanks for watching';
+$router->route('get', BASE_URI . '/about', function() {
+    echo 'about page';
 });
+
+$router->route('POST', BASE_URI . '/users/{user}/city/{city}', function($user, $city) {
+    echo 'new writing format user ' . $user . ' city = ' . $city; 
+});
+
+$router->route('GET', BASE_URI . '/company/(.*)/?', function($companyName){
+    // /? means it can has final slash or not;
+    echo 'this is company name : ' . $companyName;
+});
+
+$router->route('post', BASE_URI . '/users/(.*)/city/(.*)', function($user, $city){
+    echo 'this user is from ' . $city . ' and his/her name is ' . $user;
+});
+
+// you can use delete or patch or update also
 
 $router->dispatch($action);
 
--- src/Core/Router.php
+++ src/Core/Router.php
@@ -9,18 +9,42 @@ class Router
         $this->routers = [];
     }
 
-    public function route($action, $callback)
+    public function route($method, $action, $callback)
     {
         $action = trim($action, '/');
-        $routers[$action] = $callback;
+        if (strpos($action, "{") !== false) {
+            $action = preg_replace('/{[^}]+}/', '(.*)', $action);
+        }
+        $this->routers[$action] = ['method' => $method, 'callback' => $callback];
+    }
+
+    public function getRouters()
+    {
+        return $this->routers;
     }
 
     public function dispatch($action)
     {
         $action = trim($action, '/');
+        $sentMethod = $_SERVER['REQUEST_METHOD'];
+        $callback = null;
+        $params = [];
+        foreach ($this->routers as $router => $handler) {
+            if(preg_match("%^{$router}$%", $action, $matches)) {
+                if (strtoupper($handler['method']) == $sentMethod) {
+                    $callback = $handler['callback'];
+                    unset($matches[0]);
+                    $params = $matches;
+                    break;
+                }
+            }
+        }
 
-        $callback = $this->routers[$action];
-
-        call_user_func($callback);
+        if ($callback == null || !is_callable($callback)) {
+            http_response_code(404);
+            echo "page not found";
+        } else {
+        }
+            call_user_func($callback, ...$params);
     }
 }
